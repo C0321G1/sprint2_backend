f1f88bc87c7030b45b202bbf0d2c5fb38c91d041
truongquocphap <doitoilacanhen@gmail.com>
2021-10-07 10:17:38 +0700

    up JUnit5

---
+++ c0321g1_pawnshop_backend/src/test/java/c0321g1_pawnshop_backend/statistic/StatisticController_statisticInterest.java
@@ -0,0 +1,52 @@
+package c0321g1_pawnshop_backend.statistic;
+
+import c0321g1_pawnshop_backend.controller.statistic.StatisticRestController;
+import c0321g1_pawnshop_backend.entity.contract.Contract;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class StatisticController_statisticInterest {
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    StatisticRestController statisticRestController;
+    @Test
+    void statisticInterest_1() throws Exception {
+        this.mockMvc.perform(
+                MockMvcRequestBuilders.get("/statisticInterest"))
+                .andDo(print())
+                .andExpect(status().is4xxClientError());
+    }
+
+
+
+    @Test
+    public void statisticInterest_3() {
+        ResponseEntity<List<Contract>> listResponseEntity
+                = this.statisticRestController.listStatisticInterest(Optional.of(""),Optional.of(""));
+        List<Contract> contractList = listResponseEntity.getBody();
+        int statusCode = listResponseEntity.getStatusCodeValue();
+
+        Assertions.assertEquals(200, statusCode);
+        assert contractList != null;
+        Assertions.assertEquals("SV-0002", contractList.get(0).getContractCode());
+        Assertions.assertEquals("2021-11-12", contractList.get(0).getEndDate());
+        Assertions.assertEquals("2021-10-09", contractList.get(0).getStartDate());
+        Assertions.assertEquals("Nhung", contractList.get(0).getProductName());
+
+    }
+}
